32be4b696c09af5fd558012b07e2a56183f052d7
Benedikt Tobias <61744142+benedikttobias@users.noreply.github.com>
2022-12-06 14:07:27 +0100

    Refactored shallow models

--- algorithms/isolation_forest.py
+++ algorithms/isolation_forest.py
@@ -1,28 +1,25 @@
-from tokenize import String
-import sklearn as sk
-from sklearn.exceptions import NotFittedError
 import pandas as pd
-import numpy as np
+import sklearn as sk
 from sklearn.ensemble import IsolationForest
-from algorithms.deseason import deseasoning
+from sklearn.exceptions import NotFittedError
+
+from .util.deseason import deseasoning
 
 
 class IsolationForestModel:
-    def __init__(self, n_trees: int = 500 , nCores: int = None, cont: String = 0.01) -> None:
-        self.model = IsolationForest(n_estimators =  n_trees, n_jobs = nCores, contamination = cont)
+    def __init__(self, n_trees: int = 500, thread_amount: int = None, cont: float = 0.01) -> None:
+        self.model = IsolationForest(n_estimators=n_trees, n_jobs=thread_amount, contamination=cont)
 
     def calc_anomaly_score(self, data: pd.DataFrame) -> pd.DataFrame:
-        deseasoned = data.apply(lambda x : deseasoning(x), axis = 0)
+        deseasoned = data.apply(lambda x: deseasoning(x), axis=0)
         try:
             sk.utils.validation.check_is_fitted(self.model)
-        except NotFittedError: 
+        except NotFittedError:
             self.train_model(data)
         scores = self.model.decision_function(deseasoned.dropna())
         idx = data.iloc[336:-336].index
-        return pd.DataFrame(data = scores, index = idx)
-    
-    def train_model(self, data: pd.DataFrame):
-        deseasoned = data.apply(lambda x : deseasoning(x), axis = 0)
-        self.model.fit(deseasoned.dropna())
-    
+        return pd.DataFrame(data=scores, index=idx)
 
+    def train_model(self, data: pd.DataFrame):
+        deseasoned = data.apply(lambda x: deseasoning(x), axis=0)
+        self.model.fit(deseasoned.dropna())
